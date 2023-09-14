0f4bbe7667ca0bce5589a860ce6c7930490eed0d
Leonardo Castro Alves <leo_alves25@yahoo.com.br>
2023-09-14 10:04:15 -0300

    Início projeto

---
+++ simpleBankTransfer/src/test/java/com/simpleBankTransfer/simpleBankTransfer/SimpleBankTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleBankTransfer.simpleBankTransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
